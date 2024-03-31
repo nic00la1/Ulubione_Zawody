2eb65a0525792ec36c416ee6054e9bb3c56382f0
nic00la <nic00la.kaleta@gmail.com>
2024-03-31 15:17:24 +0200

    HTML login page

--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
