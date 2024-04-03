2fe303bd3a04dcf5b5109c7d214fc6650b44afb4
nic00la <nic00la.kaleta@gmail.com>
2024-04-03 19:57:58 +0200

    Interceptors in Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
     provideRouter(routes),
-  ]
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([authInterceptor]))
+   ]
 };
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,10 +3,11 @@ import { Employee } from '../shared/models/Employee';
 import {
   HttpClient,
   HttpErrorResponse,
+  HttpEventType,
   HttpHeaders,
   HttpParams,
 } from '@angular/common/http';
-import { Subject, catchError, map, throwError } from 'rxjs';
+import { Subject, catchError, map, tap, throwError } from 'rxjs';
 import { LoggingService } from './logging.service';
 
 @Injectable({
@@ -73,9 +74,16 @@ export class EmployeeService {
   DeleteAllEmployees() {
     this.http
       .delete(
-        'https://my-employees-24871-default-rtdb.europe-west1.firebasedatabase.app/employees.json'
+        'https://my-employees-24871-default-rtdb.europe-west1.firebasedatabase.app/employees.json',
+        { observe: 'events', responseType: 'json'}
       )
       .pipe(
+        tap((event) => {
+          console.log(event);
+          if(event.type === HttpEventType.Sent) {
+
+          }
+        }),
         catchError((err) => {
           // Write the logic to log errors
           const errorObj = {
@@ -98,26 +106,25 @@ export class EmployeeService {
     // purpose of the headers is to tell the server what kind of data we are sending
     let headers = new HttpHeaders();
     headers = headers.set('content-type', 'application/json');
-    headers = headers.append('content-type', 'text/html')
+    headers = headers.append('content-type', 'text/html');
     // the set method returns new instances after modyfing the header
     // append method appends new value to the existing header
 
     // pagination and filtering
     let queryParams = new HttpParams();
-    queryParams = queryParams.set('page', 2)
-    queryParams = queryParams.set('item', 10)
-    
+    queryParams = queryParams.set('page', 2);
+    queryParams = queryParams.set('item', 10);
 
     return this.http
       .get<{ [key: string]: Employee }>(
-        'https://my-employees-24871-default-rtdb.europe-west1.firebasedatabase.app/employees.json' 
-        , {headers: headers, params: queryParams}
+        'https://my-employees-24871-default-rtdb.europe-west1.firebasedatabase.app/employees.json',
+        { headers: headers, params: queryParams, observe: 'body' }
       )
       .pipe(
         map((res) => {
           // convert the response to an array of employees
           let employees = [];
-
+          console.log(res);
           for (let key in res) {
             if (res.hasOwnProperty(key)) {
               employees.push({ ...res[key], id: key });
