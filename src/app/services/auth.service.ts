c32e663fde71948d3bb1469de10f1c5f664852be
nic00la <nic00la.kaleta@gmail.com>
2024-04-06 19:59:15 +0200

    Creating User from Response Data | Authentication & Authorization ✅

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { AuthResponse } from '../shared/models/AuthResponse';
-import { catchError, throwError } from 'rxjs';
+import { Subject, catchError, tap, throwError } from 'rxjs';
+import { User } from '../shared/models/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
+  user = new Subject<User>();
 
   signUp(email: any, password: any) {
     const data = { email: email, password: password, returnSecureToken: true };
@@ -16,17 +18,28 @@ export class AuthService {
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBo4ZTI74hxnNFqv8D6Cmag6BLrI-Jb5Xc',
         data
       )
-      .pipe(catchError(this.handleErrors));
+      .pipe(catchError(this.handleErrors), tap((res) => {
+       this.handleCreateUser(res);
+      }));
   }
 
+  
   login(email: any, password: any) {
     const data = { email: email, password: password, returnSecureToken: true };
     return this.http.post<AuthResponse>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBo4ZTI74hxnNFqv8D6Cmag6BLrI-Jb5Xc',
       data
-    ).pipe(catchError(this.handleErrors));
+    ).pipe(catchError(this.handleErrors), tap((res) => {
+      this.handleCreateUser(res);
+    }));
   }
 
+  private handleCreateUser(res : any) {
+    const expiresInTs = new Date().getTime() + +res.expiresIn * 1000 // Timestamp for current date and time
+    const expiresIn = new Date(expiresInTs);
+    const user = new User(res.email, res.localId, res.idToken, expiresIn);
+    this.user.next(user);
+  }
   private handleErrors(err: any) {
     let errorMessage = 'Wystąpił nieznany błąd, spróbuj ponownie później';
     console.log(err);
