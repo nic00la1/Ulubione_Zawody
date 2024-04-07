ed8d5820c4686e430bd3491ab4ab311fe9e54d38
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-04-07 20:45:57 +0200

    Sending Token using Interceptor ✅

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -1,12 +1,27 @@
-import { HttpEventType, HttpInterceptorFn } from '@angular/common/http';
-import { tap } from 'rxjs';
+import {
+  HttpEventType,
+  HttpInterceptorFn,
+  HttpParams,
+} from '@angular/common/http';
+import { exhaustMap, take, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { User } from '../shared/models/User';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log('Auth intercepter called!')
-  const modifiedReq = req.clone();
-  return next(modifiedReq).pipe(tap((event) => {
-    if(event.type === HttpEventType.Response) {
-      console.log('Response arrived, body data: ', event.body);
-    }
-  }));
+  let authService = inject(AuthService);
+
+  return authService.user
+    .pipe(
+      take(1),
+      exhaustMap((user) => {
+        if(!user) {
+          return next(req);
+        }
+        const modifiedReq = req.clone({
+          params: new HttpParams().set('auth', user.token),
+        })
+        return next(modifiedReq);
+      })
+    )
 };
