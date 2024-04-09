fc2624a881f6fec4185bef2c73dae4fdf43040e2
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-04-09 10:32:11 +0000

    new Home Page ✨

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,10 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
